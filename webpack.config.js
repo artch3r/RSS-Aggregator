b682716a7f88360aebecb637851a4369285bba97
Артем Черных <acac1212@yandex.ru>
2022-12-09 17:09:54 +1000

    install Bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,29 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env'],
+                  },
+                },
+              },
+              { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+              {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+              },
+              {
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000',
+              },
+              {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
+              },
             // Add your rules for custom modules here
             // Learn more about loaders from https://webpack.js.org/loaders/
         ],
