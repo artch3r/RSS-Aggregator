f86e10a0b37c17e6c09411de2c9ee22911eb5ad9
Артем Черных <acac1212@yandex.ru>
2022-12-13 11:56:48 +1000

    remove async/await from index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import i18next from 'i18next';
 import app from './app.js';
 import resources from './locales/index.js';
 
-const runApp = async () => {
+const runApp = () => {
   const i18nextInstance = i18next.createInstance();
-  await i18nextInstance.init({
+  i18nextInstance.init({
     lng: 'ru',
     debug: false,
     resources,
-  });
-
-  app(i18nextInstance);
+  })
+    .then(() => {
+      app(i18nextInstance);
+    });
 };
 
 runApp();
